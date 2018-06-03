47090002e210cff6899277bf696e6733e7e39333
zeniab91 <zeniab91@users.noreply.github.com>
2018-06-03 00:13:36 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,16 @@
-## R Programming Assignment 2
-## Week 3
+## Put comments here that give an overall description of what your
+## functions do
+
+## create an invertible matrix
+## return a list containing functions to set and get the matrix, and to set and get the inverse
+## this list is used as the input to the cacheSolve() function
+## the cacheSolve() function then computes the inverse of the matrix returned by the makeCacheMatrix function
+## if the inverse has been calculated, cacheSolve retrieves the inverse from the cache
+## if the inverse has not been calculated, cacheSolve will calculate and set the value in the cache, then print the result
+
+
+
+## Write a short comment describing this function
 
 ## Creates a special "matrix" object that can cache its inverse
 
@@ -17,11 +28,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 }
 
+## Write a short comment describing this function
 
-## Computes the inverse of the special "matrix" returned by makeCacheMatrix. If the inverse has already been 
-## calculated, then the cacheSolve retrieves the inverse from the cache.
+## Computes the inverse of the matrix returned by makeCacheMatrix. If the inverse has already been 
+## calculated, then the cacheSolve function retrieves the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
         c <- x$getinv()     
       
         if(!is.null(c)){        ## if the inverse has already been calculated
@@ -40,3 +53,6 @@ cacheSolve <- function(x, ...) {
         ## print result
         c
 }
+
+
+
